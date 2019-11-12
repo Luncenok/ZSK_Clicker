3023c74c464beaf20d3d9bbe2da9343d1b052195
PanTajemnic <idziejczak3@gmail.com>
2019-11-12 22:14:37 +0100

    - osobno liczone kliki i coinsy - dopracowane zmienianie kolorów - nowe elementy graficzne (tory) -

--- app/src/main/java/mateusz/idziejczak/zskclicker/ui/home/HomeFragment.java
+++ app/src/main/java/mateusz/idziejczak/zskclicker/ui/home/HomeFragment.java
@@ -13,6 +13,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -27,8 +28,8 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private Animation animation, animation2, animation3, animationTrain;
-    private int clicks;
-    private long cps=0;
+    private int clicks = 0, delay = 1000, coins;
+    private long cps = 0, cpc = 1, secondsBonus;
     private SharedPreferences.Editor editor;
     private SharedPreferences sharedPref;
 
@@ -40,73 +41,120 @@ public class HomeFragment extends Fragment {
         final TextView coinsTV = root.findViewById(R.id.text_home);
         final TextView cpsTV = root.findViewById(R.id.text_home_2);
         final ProgressBar progressBar = root.findViewById(R.id.progressBar);
+        final ConstraintLayout klikerLayout = root.findViewById(R.id.layoutKlikania);
+        final ImageView coalTankIV = root.findViewById(R.id.coalTankHomeIV);
+        final ImageView tankIV = root.findViewById(R.id.tankHomeIV);
+        final ImageView trainIV = root.findViewById(R.id.trainHomeIV);
+        final ImageView coalIV = root.findViewById(R.id.coalHomeIV);
+        final ImageView railsIV = root.findViewById(R.id.railsIV);
+        railsIV.setColorFilter(Color.rgb(140,140,140));
+        trainIV.setColorFilter(Color.rgb(56, 31, 0));
+        tankIV.setColorFilter(Color.rgb(56, 31, 0));
+        final ImageView coal2IV = root.findViewById(R.id.coalHome2IV);
+        final ImageView coal3IV = root.findViewById(R.id.coalHome3IV);
 
         animation = AnimationUtils.loadAnimation(getContext(), R.anim.wegielzaklik);
         animation2 = AnimationUtils.loadAnimation(getContext(), R.anim.wegielzaklik);
         animation3 = AnimationUtils.loadAnimation(getContext(), R.anim.wegielzaklik);
         animationTrain = AnimationUtils.loadAnimation(getContext(), R.anim.trainmove);
 
-        Context context = getActivity();
+        final Context context = getActivity();
         sharedPref = context.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         editor = sharedPref.edit();
-        clicks = sharedPref.getInt(getString(R.string.saved_coins_key), 1);
+        coins = sharedPref.getInt(getString(R.string.saved_coins_key), 1);
+        clicks = sharedPref.getInt(getString(R.string.saved_clicks_key), 0);
         int trainNumber = sharedPref.getInt(getString(R.string.saved_train_key), 1);
-        coinsTV.setText(String.format(Locale.getDefault(), "coins: %d", clicks, cps));
-
-        ConstraintLayout klikerLayout = root.findViewById(R.id.layoutKlikania);
-        final ImageView coalTankIV = root.findViewById(R.id.coalTankHomeIV);
-        final ImageView tankIV = root.findViewById(R.id.tankHomeIV);
-        final ImageView trainIV = root.findViewById(R.id.trainHomeIV);
-        final ImageView coalIV = root.findViewById(R.id.coalHomeIV);
-        final ImageView coal2IV = root.findViewById(R.id.coalHome2IV);
-        final ImageView coal3IV = root.findViewById(R.id.coalHome3IV);
-
-        if(trainNumber ==1) trainIV.setImageResource(R.drawable.ic_train_svgrepo_com);
-        if(trainNumber ==2) trainIV.setImageResource(R.drawable.ic_train_svgrepo_com_03);
-
+        cpc = sharedPref.getInt(getString(R.string.saved_cpc_key), 1);
         progressBar.setProgress(clicks);
+        progressBar.getProgressDrawable().setColorFilter(
+                getResources().getColor(R.color.colorPrimary), android.graphics.PorterDuff.Mode.SRC_IN);
+        coinsTV.setText(String.format(Locale.getDefault(), "coins: %d", coins));
+
+        if (trainNumber == 1) trainIV.setImageResource(R.drawable.ic_train_svgrepo_com);
+        if (trainNumber == 2) trainIV.setImageResource(R.drawable.ic_train_svgrepo_com_03);
 
         klikerLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (clicks % 3 == 0) {
-                    coal3IV.setVisibility(View.INVISIBLE);
-                    coalIV.setVisibility(View.VISIBLE);
-                    coalIV.startAnimation(animation);
-                } else if (clicks % 3 == 1) {
-                    coalIV.setVisibility(View.INVISIBLE);
-                    coal2IV.setVisibility(View.VISIBLE);
-                    coal2IV.startAnimation(animation2);
-                } else if (clicks % 3 == 2) {
-                    coal2IV.setVisibility(View.INVISIBLE);
-                    coal3IV.setVisibility(View.VISIBLE);
-                    coal3IV.startAnimation(animation3);
+                if (cpc != 0) {
+
+                    if (clicks % 3 == 0) {
+                        coal3IV.setVisibility(View.INVISIBLE);
+                        coalIV.setVisibility(View.VISIBLE);
+                        coalIV.startAnimation(animation);
+                    } else if (clicks % 3 == 1) {
+                        coalIV.setVisibility(View.INVISIBLE);
+                        coal2IV.setVisibility(View.VISIBLE);
+                        coal2IV.startAnimation(animation2);
+                    } else if (clicks % 3 == 2) {
+                        coal2IV.setVisibility(View.INVISIBLE);
+                        coal3IV.setVisibility(View.VISIBLE);
+                        coal3IV.startAnimation(animation3);
+                    }
+                    clicks++;
+                    coins += cpc;
+                    cps++;
+                    editor.putInt(getString(R.string.saved_coins_key), coins).apply();
+                    editor.putInt(getString(R.string.saved_clicks_key), clicks).apply();
+                    coinsTV.setText(String.format(Locale.getDefault(), "coins: %d", coins));
+                    progressBar.setProgress(clicks);
+                } else {
+                    Toast.makeText(context, "The train is overheated. Please wait 5 seconds until it cools", Toast.LENGTH_SHORT).show();
+                    delay = 1000;
                 }
-                clicks++;
-                cps++;
-                coinsTV.setText(String.format(Locale.getDefault(), "coins: %d", clicks));
-                progressBar.setProgress(clicks);
-                progressBar.getProgressDrawable().setColorFilter(
-                        Color.BLACK, android.graphics.PorterDuff.Mode.SRC_IN);
+
+                if (clicks >= 500) {
+                    progressBar.setMax(1000);
+                    progressBar.setProgress(clicks);
+                }
+                if (clicks >= 1000) {
+                    progressBar.setMax(2000);
+                    progressBar.setProgress(clicks);
+                }
+                if (clicks >= 2000) {
+                    progressBar.setMax(4000);
+                    progressBar.setProgress(clicks);
+                }
+                if (clicks >= 4000) {
+                    progressBar.setMax(progressBar.getMax()*2);
+                    progressBar.setProgress(clicks);
+                }
             }
         });
 
         final Handler handler = new Handler();
-        final int delay = 1000;
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                cpsTV.setText(String.format(Locale.getDefault(), "cps: %d ",cps));
+                cpsTV.setText(String.format(Locale.getDefault(), "cps: %d ", cps));
 
-                if(cps>0){
+                if (cps > 0) {
                     tankIV.startAnimation(animationTrain);
                     trainIV.startAnimation(animationTrain);
-                }
-                if (clicks >= 2000) {
-                    progressBar.setMax(4000);
+                    secondsBonus++;
+                } else {
+                    secondsBonus = 0;
                 }
 
-                cps=0;
+                if (secondsBonus <= 0) {
+                    trainIV.setColorFilter(Color.rgb(56, 31, 0));
+                    tankIV.setColorFilter(Color.rgb(56, 31, 0));
+                    cpc = 1;
+                } else if (secondsBonus >= 5 && secondsBonus < 10) {
+                    trainIV.setColorFilter(Color.rgb(130, 73, 1));
+                    tankIV.setColorFilter(Color.rgb(130, 73, 1));
+                } else if (secondsBonus >= 10 && secondsBonus < 15) {
+                    trainIV.setColorFilter(Color.rgb(161, 71, 3));
+                    tankIV.setColorFilter(Color.rgb(161, 71, 3));
+                    cpc = 2;
+                } else if (secondsBonus >= 15) {
+                    delay = 5000;
+                    trainIV.setColorFilter(Color.rgb(194, 55, 0));
+                    tankIV.setColorFilter(Color.rgb(194, 55, 0));
+                    cpc = 0;
+                }
+
+                cps = 0;
                 handler.postDelayed(this, delay);
             }
         }, delay);
@@ -116,19 +164,19 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onPause() {
-        editor.putInt(getString(R.string.saved_coins_key), clicks).commit();
+        editor.putInt(getString(R.string.saved_coins_key), coins).commit();
         super.onPause();
     }
 
     @Override
     public void onStop() {
-        editor.putInt(getString(R.string.saved_coins_key), clicks).commit();
+        editor.putInt(getString(R.string.saved_coins_key), coins).commit();
         super.onStop();
     }
 
     @Override
     public void onDestroy() {
-        editor.putInt(getString(R.string.saved_coins_key), clicks).commit();
+        editor.putInt(getString(R.string.saved_coins_key), coins).commit();
         super.onDestroy();
     }
 }
